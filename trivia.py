e6ffa2d98f503cc96d32b3250af7fe6e66be7280
Alkerg <albert.arg500@gmail.com>
2025-04-11 18:12:13 -0500

    Implementacion de la clase Quiz y flujo basico del juego

--- trivia.py
+++ trivia.py
@@ -6,3 +6,37 @@ class Question:
 
     def is_correct(self, answer):
         return self.correct_answer == answer
+    
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
+    
+def run_quiz():
+    print("Bienvenido al juego de Trivia!")
+    print("Responde las siguientes preguntas seleccionando el número de la opción correcta.")
+    quiz = Quiz()
+    quiz.add_question(Question('Cuál es la capital de Francia?', ['Londres', 'Berlín', 'París', 'Madrid'], 'París'))
+    quiz.add_question(Question('Cuanto es 2 + 2?', ['3', '4', '5', '6'], '4'))
+    quiz.add_question(Question('Cuantos huesos tiene el cuerpo humano?', ['206', '205', '204', '203'], '206'))
+    
+    while quiz.current_question_index < 10:
+        question = quiz.get_next_question()
+        if question:
+            print(f"Pregunta {quiz.current_question_index}: {question.description}")
+            for idx, option in enumerate(question.options):
+                print(f"{idx + 1}) {option}")
+            answer = input("Tu respuesta: ")
+        else:
+            break
+    print("Juego terminado.")
